64fad0c1aa31496eef81fa0c19bfee2ed5e0e8e6
Joe Bew <joebew42@gmail.com>
2021-11-18 23:24:07 +0100

    Inline Order constructor

--- app/src/main/java/io/vocidelcodice/orders_and_registrations/Order.java
+++ app/src/main/java/io/vocidelcodice/orders_and_registrations/Order.java
@@ -6,10 +6,12 @@ import java.util.UUID;
 public record Order(UUID orderId, UUID conferenceId, Integer numberOfSeats, List<Event> changes) {
 
     static Order place(UUID orderId, UUID conferenceId, Integer numberOfSeats) {
-        return new Order(orderId, conferenceId, numberOfSeats);
+        return new Order(
+                orderId,
+                conferenceId,
+                numberOfSeats,
+                List.of(new OrderPlaced(orderId, conferenceId, numberOfSeats))
+        );
     }
 
-    Order(UUID orderId, UUID conferenceId, Integer numberOfSeats) {
-        this(orderId, conferenceId, numberOfSeats, List.of(new OrderPlaced(orderId, conferenceId, numberOfSeats)));
-    }
 }
