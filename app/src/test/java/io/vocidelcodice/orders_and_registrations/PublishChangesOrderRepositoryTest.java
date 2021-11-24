2d72a7abf7fb0f0af19596e7c9dacad903e7274e
Matteo Pierro <pierromatteo@gmail.com>
2021-11-24 23:38:41 +0200

    wip

---
+++ app/src/test/java/io/vocidelcodice/orders_and_registrations/PublishChangesOrderRepositoryTest.java
@@ -0,0 +1,11 @@
+package io.vocidelcodice.orders_and_registrations;
+
+import org.junit.jupiter.api.Test;
+
+public class PublishChangesOrderRepositoryTest {
+
+    @Test
+    void xyz() {
+        
+    }
+}
